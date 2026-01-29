e22003cecbdb1f1347643cfe1c8006b01d4a1e90
scottbtw <akyltai13@gmail.com>
2026-01-29 12:14:22 +0500

    Update Graph.h

--- rcp/Graph.h
+++ rcp/Graph.h
@@ -1,1 +1,28 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <map>
+
+class Graph {
+private:
+    std::map<int, std::vector<int>> adj;
+    bool directed;
+    int edges;
+
+public:
+    Graph(bool directed = false);
+
+    void addVertex(int v);
+    void addEdge(int u, int v);
+
+    std::vector<int> neighbors(int v) const;
+
+    int V() const;
+    int E() const;
+
+    bool isDirected() const;
+};
+
+#endif
 
