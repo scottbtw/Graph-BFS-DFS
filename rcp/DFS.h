96a5cad8d32d38ee887737bf49e1b70abe168822
scottbtw <akyltai13@gmail.com>
2026-01-29 12:15:16 +0500

    Update DFS.h

--- rcp/DFS.h
+++ rcp/DFS.h
@@ -1,1 +1,12 @@
+#ifndef DFS_H
+#define DFS_H
 
+#include "Graph.h"
+#include <vector>
+#include <set>
+
+void dfsRecursive(const Graph& g, int v, std::set<int>& visited, std::vector<int>& order);
+std::vector<int> dfsIterative(const Graph& g, int start);
+bool hasCycle(const Graph& g, int v, int parent, std::set<int>& visited);
+
+#endif
